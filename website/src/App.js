724b15717fafd9affa78f2aa66a81293f2f41941
unknown <pbplop@gmail.com>
2022-10-30 19:54:44 +0530

    initiated

---
+++ website/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Navmenu from "./Components/Navmenu";
+function App() {
+  return (
+    <div className="App">
+      <Navmenu />
+      <h1>WASSUP</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ website/src/Components/Navmenu.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Navmenu() {
+  return <div></div>;
+}
+
+export default Navmenu;
