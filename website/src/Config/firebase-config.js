92aa5a2de8932040cbf1edcc14324efaac713a4f
unknown <pbplop@gmail.com>
2022-10-31 10:21:58 +0530

    basic updation

---
+++ website/src/Config/firebase-config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAFQ48nfVeVFQKLllz6fvct8rxqPqF-8Ek",
+  authDomain: "nodetest-66e6e.firebaseapp.com",
+  databaseURL:
+    "https://nodetest-66e6e-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "nodetest-66e6e",
+  storageBucket: "nodetest-66e6e.appspot.com",
+  messagingSenderId: "459524848079",
+  appId: "1:459524848079:web:61703aa0b8f560c1c0faba",
+  measurementId: "G-LVE99ZH95V",
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
