ddb86a12c8a21dbff1acee7eeec83573c980d7e8
unknown <pbplop@gmail.com>
2022-11-10 10:13:31 +0530

    started nov 10

--- website/src/Pages/Statistics.js
+++ website/src/Pages/Statistics.js
@@ -4,27 +4,7 @@ import { db } from "../Config/firebase-config";
 import "../Styles/pages.css";
 
 function Statistics() {
-  const [info, setInfo] = useState([]);
-
-  useEffect(() => {
-    const infoRef = db.database().ref("SpO2");
-    infoRef.on("value", (snapshot) => {
-      const data = snapshot.val();
-      const infoList = [];
-      for (let id in data) {
-        infoList.push({ id, ...data[id] });
-      }
-      setInfo(infoList);
-    });
-  }, []);
-
-  return (
-    <div>
-      {info.map((info) => {
-        <>{info.message}</>;
-      })}
-    </div>
-  );
+  return <div>hello</div>;
 }
 
 export default Statistics;
